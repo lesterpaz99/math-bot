abf60a4c9a778caed7f24369ceca79dd2c9ab67b
Obed Paz <lesterpaz99@gmail.com>
2021-07-08 13:52:22 -0600

    functions to calculate the diameter, perimeter and area of a circle

--- app.js
+++ app.js
@@ -8,3 +8,17 @@ const trianglePerimeter = (side1, side2, base) => {
 };
 
 const triangleArea = (base, height) => (base * height) / 2;
+
+// CIRCLE
+const PI = Math.PI;
+
+const circleDiameter = (radio) => radio * 2;
+
+const circlePerimeter = (radio) => {
+	const diameter = circleDiameter(radio);
+	return Number((diameter * PI).toFixed(2));
+};
+
+const circleArea = (radio) => {
+	return Number((radio ** 2 * PI).toFixed(2));
+};
