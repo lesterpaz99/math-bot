9472ea5697085fbdf3f06fbe34c3803fa8c33974
Obed Paz <lesterpaz99@gmail.com>
2021-07-08 13:50:09 -0600

    functions to calculate the perimeter and the area of a triangle

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 // SQUARE
-
 const squarePerimeter = (side) => side * 4;
 const squareArea = (side) => side ** 2;
+
+// TRIANGLE
+const trianglePerimeter = (side1, side2, base) => {
+	return side1 + side2 + base;
+};
+
+const triangleArea = (base, height) => (base * height) / 2;
