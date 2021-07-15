d319ed14811c865d769b6209d8483238a711f959
Obed Paz <lesterpaz99@gmail.com>
2021-07-14 23:39:41 -0600

    create the addEventListener functions for the buttons

--- main.js
+++ main.js
@@ -57,6 +57,44 @@ startingPrice.addEventListener('change', getFinalPrice);
 discount.addEventListener('keyup', getFinalPrice);
 discount.addEventListener('change', getFinalPrice);
 
-let arr = [70, 100, 70, 100, 100, 20, 80, 100, 80, 70, 70];
+// STATISTICS
+const meanBtn = document.getElementById('mean-btn');
+const medianBtn = document.getElementById('median-btn');
+const modeBtn = document.getElementById('mode-btn');
 
-console.log(getMode(arr));
+const stopDefaultAction = (ev) => ev.preventDefault();
+
+const callToAction = (ev, tool) => {
+	stopDefaultAction(ev);
+	let data = document.getElementById('statistics-data').value.trim();
+	const answer = document.getElementById('statistics-result');
+
+	if (data === '') {
+		answer.innerText = 'Enter a valid data';
+		return answer;
+	}
+
+	data = data.split('-');
+	data = data.map((element) => Number(element));
+
+	const resultP = document.getElementById('result-chosen');
+	resultP.innerText = tool;
+
+	let result = 0;
+	const isMean = () => tool.toLowerCase() === 'mean';
+	const isMedian = () => tool.toLowerCase() === 'median';
+	const isMode = () => tool.toLowerCase() === 'mode';
+
+	if (isMean()) result = getAverage(data);
+	if (isMedian()) result = getMedian(data);
+	if (isMode()) {
+		result = getMode(data);
+		answer.innerText = result;
+		return answer;
+	}
+	answer.innerText = isNaN(result) ? 'Enter a valid data' : result;
+};
+
+meanBtn.addEventListener('click', (e) => callToAction(e, 'Mean'));
+medianBtn.addEventListener('click', (e) => callToAction(e, 'Median'));
+modeBtn.addEventListener('click', (e) => callToAction(e, 'Mode'));
